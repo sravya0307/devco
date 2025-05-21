48989b7529af0ac2fffade72c56cd2bef23baea5
sravya0307 <kadiyalasravyasri@gmail.com>
2025-05-21 17:33:51 +0530

    intialization with next and setup firebase

---
+++ hackmate/src/app/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDDACm9jrw5xAFOF7Y5iNVVWWNAOQvgCUI",
+    authDomain: "devco-628d1.firebaseapp.com",
+    projectId: "devco-628d1",
+    storageBucket: "devco-628d1.firebasestorage.app",
+    messagingSenderId: "464866012829",
+    appId: "1:464866012829:web:79059be7786bcd37e76e74",
+    measurementId: "G-MV6CSS76L9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
